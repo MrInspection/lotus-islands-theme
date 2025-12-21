497c86907f294447dc957274d35fbff964a84d18
Moussa <moussaabdallahhoumadi@gmail.com>
2025-12-21 12:36:39 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lotus Islands Theme
+
+![JetBrains Plugin](https://img.shields.io/badge/JetBrains-2025.3+-000000?style=flat&logo=jetbrains&logoColor=white)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+The beloved **Lotus Theme**, beautifully adapted for the new **JetBrains Islands UI** (2025.3+). This theme contains colors from [Rainbow Brackets](https://plugins.jetbrains.com/plugin/10080-rainbow-brackets).
+
+
---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("java")
+    id("org.jetbrains.intellij.platform") version "2.10.5"
+}
+
+group = "fr.moussax"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdea("2025.3")
+        bundledPlugin("com.intellij.java")
+        testFramework(TestFrameworkType.Platform)
+    }
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
