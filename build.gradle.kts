73c685d4e68be796a0aba537ef16d1143d212afa
Moussa <155066673+MrInspection@users.noreply.github.com>
2026-07-01 15:30:35 +0200

    fix: patch glitch rectangle around buttons

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "fr.moussax"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        intellijIdea("2026.1")
+        intellijIdea("2025.3")
         bundledPlugin("com.intellij.java")
         testFramework(TestFrameworkType.Platform)
     }
